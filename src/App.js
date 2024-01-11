14555d7576a1c7f93cb96cbd5fadd249b7e786ff
Awais7ad <132563972+Awais7ad@users.noreply.github.com>
2024-01-11 17:57:39 +0500

    file delete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import React, { useEffect, useState } from "react";
-// import { Container} from 'styled-bootstrap-grid';
-// import {  Route,Routes, useNavigate } from "react-router-dom";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Product from "./pages/Products";
+import React from "react";
+
 import Calculator from "./Calculator";
-import Form from "./Form";
-import { getAllProducts } from "./api/api";
+
+
 
 const App = () => {
 
-// useEffect(async()=>{
-//   const getProducts =await getAllProducts()
-//   console.log("getProducts",getProducts)
-// },[])
-  // const navigate =useNavigate()
-  // navigate("/home")
+
   return (
     <>
-    <Form/>
     <Calculator/>
-      {/* <Form/> */}
-      {/* <Routes> 
-   <Route path="/home" element={<Home/>}></Route>
-   <Route path="/" element={<About/>}></Route>
-   <Route path="/product" element={<Product/>}></Route>
-</Routes>   */}
     </>
   );
 };
